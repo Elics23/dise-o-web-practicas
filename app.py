a7aa72c0b0cf36109b9ba48c3957314e848c0eac
Elics23 <fp23ifcb0126@aulatomillo.org>
2025-05-20 15:21:16 +0100

    diseños

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/galeria')
+def galeria():
+    return render_template('diseno.html')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
